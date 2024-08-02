b12c742f1f46912bc6072aa631441890144eb6a2
IAmBamboo <ChimeiPho@gmail.com>
2024-08-02 01:38:13 -0600

    Big update to log_entry_view
    integrate FoodProduct class
    integrate _buildList from home.dart
    sort parse by popularity
    get nutrition facts from parse and make a FoodProduct from it
    modify search results to use _buildList

--- calorie_tracker_app/lib/pages/log_entry_view.dart
+++ calorie_tracker_app/lib/pages/log_entry_view.dart
@@ -1,4 +1,5 @@
 import "package:calorie_tracker_app/app_state.dart";
+import "package:calorie_tracker_app/models/food_product.dart";
 import "package:calorie_tracker_app/models/log.dart";
 import "package:flutter/material.dart";
 import 'package:http/http.dart' as http;
@@ -23,9 +24,10 @@ class _LogEntryViewState extends State<LogEntryView> {
 
   // Search state variables
   String _searchQuery = '';
-  List<dynamic> _searchResults = [];
+  List<FoodProduct> _searchResults = [];
   String _searchMessage = '';
   bool _isLoading = false;
+  int? _expandedTile; //Used to control which ListTile is expanded
 
   @override
   void initState() {
@@ -53,19 +55,33 @@ class _LogEntryViewState extends State<LogEntryView> {
     });
 
     final response = await http.get(
-      Uri.parse('https://world.openfoodfacts.org/cgi/search.pl?search_terms=$query&search_simple=1&action=process&json=1'),
+      Uri.parse('https://world.openfoodfacts.org/cgi/search.pl?search_terms=$query&search_simple=1&action=process&json=1&sort_by=unique_scans_n'),
     );
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
+      final products = data['products'] ?? [];
+      final List<FoodProduct> searchResults = products.map<FoodProduct>((product) {
+        final nutriments = product['nutriments'] ?? {};
+        final double? calories = nutriments.containsKey('energy-kcal_serving') ? nutriments['energy-kcal_serving'].toDouble() : null;
+        final String? servingSize = product['serving_size'];
+        final String productName = product['product_name'] ?? 'Unknown product';
+        return FoodProduct(name: productName, calories: calories, servingSize: servingSize);
+      }).toList();
+
       setState(() {
-        _searchResults = data['products'] ?? [];
+        _searchResults = searchResults;
         _searchMessage = 'Search found ${_searchResults.length} products';
+        _isLoading = false;
+      });
+    } else if (response.statusCode == 500 || response.statusCode == 502 || response.statusCode == 503) {
+      setState(() {
+        _searchMessage = 'Error code: ${response.statusCode}, OpenFoodFacts server is down. Please wait awhile';
         _isLoading = false;
       });
     } else {
       setState(() {
-        _searchMessage = 'Error fetching search results';
+        _searchMessage = 'Error fetching search results, code: ${response.statusCode}';
         _isLoading = false;
       });
       // Handle
@@ -154,13 +170,12 @@ class _LogEntryViewState extends State<LogEntryView> {
                 if (_isLoading)
                   const CircularProgressIndicator()
                 else
-                  Text(_searchMessage, style: const TextStyle(color: Colors.white)),
-                  // Expanded(
-                  //   child: SingleChildScrollView(
-                  //     //This is the TextField for the Description/Content of the Note document
-                  //     child: Text('List')
-                  //   ),
-                  // ),
+                  Expanded(
+                    child: _searchResults.isEmpty 
+                      ? const Center(child: Text('No results found', style: TextStyle(color: Colors.white))) 
+                      // BUILDING A LIST TO DISPLAY SEARCH RESULTS
+                      : _buildList(_searchResults)
+                    ),
               if (!_isNewLog) 
               // THIS IS A NOT NEW LOG, SHOW CONTENT OF THE LOG ITEM
               Column(
@@ -204,4 +219,116 @@ class _LogEntryViewState extends State<LogEntryView> {
       );
     }
   }
+
+  /// Builds a list of Log widgets.
+  /// Each list item is a GestureDetector that when tapped, expands the object to show the full food entry Log description.
+  /// An AnimatedSize widget is used to animate the expansion and contraction of the list items.
+  /// Each list item includes a ListTile displaying the food log entry's name and calories, an icon
+  /// indicating its expansion state, and a PopupMenuButton with options to edit or delete the log.
+  ///
+  /// Arguments:
+  /// - logs: A List<Log> containing the logs to be displayed.
+  Widget _buildList(List<FoodProduct> query) {
+    return ListView.builder(
+      itemCount: query.length,
+      itemBuilder: (context, index) {
+        final FoodProduct product = query[index];
+        final bool isExpanded = _expandedTile == index;
+
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              _expandedTile = isExpanded ? null : index; //Toggle expansion state
+              Feedback.forTap(context);
+              //Handle
+            });
+          },
+          child: AnimatedSize(
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeOutQuint,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: isExpanded ? const Color.fromARGB(255, 255, 228, 141) : Colors.grey,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      product.name, 
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 255, 242, 199),
+                        shadows: <Shadow>[
+                          Shadow(
+                            blurRadius: 2,
+                            color: Color.fromARGB(255, 255, 228, 141),
+                          ),
+                        ],
+                      ),
+                    ),
+                    subtitle: Text('${product.calories?.toString() ?? 'Unknown'} cals per serving of ${product.servingSize ?? 'Unknown'}', 
+                      maxLines: isExpanded ? null : 1, 
+                      overflow: isExpanded ? TextOverflow.visible : TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 190, 190, 190),
+                      ),
+                    ),
+                    leading: isExpanded ? 
+                    const Icon(Icons.keyboard_arrow_up_rounded, 
+                      color: Color.fromARGB(255, 255, 196, 0),
+                      shadows: <Shadow>[
+                        Shadow(
+                          blurRadius: 5,
+                          color: Color.fromARGB(255, 255, 196, 0),
+                        ),
+                      ],
+                    )
+                    : const Icon(Icons.keyboard_arrow_down_rounded, 
+                      color: Color.fromARGB(255, 255, 196, 0),
+                      shadows: <Shadow>[
+                        Shadow(
+                          blurRadius: 5,
+                          color: Color.fromARGB(255, 255, 196, 0),
+                        ),
+                      ],
+                    ),
+                    trailing: PopupMenuButton(
+                      iconColor: const Color.fromARGB(255, 255, 196, 0),
+                      onSelected: (value) {
+                        switch (value) {
+                          case 'open':
+                            print('open search result');
+                            break;
+                          // case 'delete':
+                          //   print('delete search result')
+                          //   break;
+                        }
+                      },
+                      itemBuilder: (BuildContext bc) {
+                        return const [
+                          PopupMenuItem(
+                            value: 'open',
+                            child: Text("View Product"),
+                          ),
+                          // PopupMenuItem(
+                          //   value: 'delete',
+                          //   child: Text("Delete"),
+                          // ),
+                        ];
+                      },
+                    ),
+                  )
+                ],
+              ),
+            )
+          )
+        );
+      },
+    );
+  }
+
 }
