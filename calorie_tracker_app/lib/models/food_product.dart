2aab88ada6468fa81afa36febd4230ec79740558
IAmBamboo <ChimeiPho@gmail.com>
2024-08-02 01:22:59 -0600

    Create food_product.dart

---
+++ calorie_tracker_app/lib/models/food_product.dart
@@ -0,0 +1,11 @@
+class FoodProduct {
+  FoodProduct({
+    required this.calories,
+    required this.servingSize,
+    required this.name,
+    });
+
+  final String name;
+  final double? calories;
+  final String? servingSize;
+}
