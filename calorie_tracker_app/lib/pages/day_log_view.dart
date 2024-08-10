d2bd99b73c3a074120ea85caed948f8134999a39
IAmBamboo <ChimeiPho@gmail.com>
2024-08-09 23:41:44 -0600

    move list to _buildlist, separate by eatTime, add calorie counters to appbar

--- calorie_tracker_app/lib/pages/day_log_view.dart
+++ calorie_tracker_app/lib/pages/day_log_view.dart
@@ -1,11 +1,12 @@
 import 'package:calorie_tracker_app/app_state.dart';
+import 'package:calorie_tracker_app/models/log.dart';
 import 'package:calorie_tracker_app/pages/log_entry_view.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class DayLogView extends StatelessWidget {
   final AppState appState;
-  final List logsList;
+  final List<Log> logsList;
   final int? expandedTile;
   final void Function(int) onTileTap;
   final void Function(int, String) onLogAction;
@@ -21,6 +22,7 @@ class DayLogView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int totalCalories = logsList.fold(0, (sum, log) => sum + log.calories);
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
         floatingActionButton: FloatingActionButton(
@@ -83,18 +85,81 @@ class DayLogView extends StatelessWidget {
           ),
         ],
         bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(30.0),
+          preferredSize: const Size.fromHeight(60.0),
           child: Container(
-            color: Colors.red,
-            height: 30.0,
+            color: const Color.fromARGB(255, 31, 31, 31),
             width: double.infinity,
             alignment: Alignment.center,
-            child: const Text(
-              "Calories Consumed: 0",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Budget",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 242, 199),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    Text(
+                      "0",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 40.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Consumed",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 242, 199),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0, 
+                      ),
+                    ),
+                    Text(
+                      "$totalCalories",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0, 
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 40.0),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Limit",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 242, 199),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    Text(
+                      "1800",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
@@ -114,119 +179,157 @@ class DayLogView extends StatelessWidget {
                 ),
               ),
             )
-          : ListView.builder(
-              itemCount: logsList.length,
-              itemBuilder: (context, index) {
-                final log = logsList[index];
-                final bool isExpanded = expandedTile == index;
+          : _buildList(context, logsList)
+    );
+  }
 
-                return GestureDetector(
-                  onTap: () => onTileTap(index),
-                  child: AnimatedSize(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeOutQuint,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            color: isExpanded
-                                ? const Color.fromARGB(255, 255, 228, 141)
-                                : Colors.grey,
-                            width: 2.0,
-                          ),
-                        ),
-                      ),
-                      child: Column(
-                        children: [
-                          ListTile(
-                            key: UniqueKey(),
-                            title: Text(
-                              log.name,
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 255, 242, 199),
-                                shadows: <Shadow>[
-                                  Shadow(
-                                    blurRadius: 2,
-                                    color: Color.fromARGB(255, 255, 228, 141),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            subtitle: Text(
-                              'Calories: ${log.calories.toString()}',
-                              maxLines: isExpanded ? null : 1,
-                              overflow: isExpanded
-                                  ? TextOverflow.visible
-                                  : TextOverflow.ellipsis,
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 190, 190, 190),
-                              ),
-                            ),
-                            leading: isExpanded
-                                ? const Icon(
-                                    Icons.keyboard_arrow_up_rounded,
-                                    color: Color.fromARGB(255, 255, 196, 0),
-                                    shadows: <Shadow>[
-                                      Shadow(
-                                        blurRadius: 5,
-                                        color: Color.fromARGB(255, 255, 196, 0),
-                                      ),
-                                    ],
-                                  )
-                                : const Icon(
-                                    Icons.keyboard_arrow_down_rounded,
-                                    color: Color.fromARGB(255, 255, 196, 0),
-                                    shadows: <Shadow>[
-                                      Shadow(
-                                        blurRadius: 5,
-                                        color: Color.fromARGB(255, 255, 196, 0),
-                                      ),
-                                    ],
-                                  ),
-                            trailing: PopupMenuButton(
-                              iconColor: const Color.fromARGB(255, 255, 196, 0),
-                              onSelected: (value) {
-                                switch (value) {
-                                  case 'open':
-                                    Navigator.of(context).push(
-                                      MaterialPageRoute(
-                                        builder: (context) => LogEntryView(log: log, appState: appState),
-                                      ),
-                                    );
-                                    break;
-                                  case 'delete':
-                                    appState.deleteLog(
-                                      log: log,
-                                      onSuccess: () {
-                                        ScaffoldMessenger.of(context).showSnackBar(
-                                          const SnackBar(content: Text('Log was successfully deleted.')),
-                                        );
-                                      }
-                                    );
-                                    break;
-                                }
-                              },
-                              itemBuilder: (BuildContext bc) {
-                                return const [
-                                  PopupMenuItem(
-                                    value: 'open',
-                                    child: Text("View Log"),
-                                  ),
-                                  PopupMenuItem(
-                                    value: 'delete',
-                                    child: Text("Delete"),
-                                  ),
-                                ];
-                              },
-                            ),
-                          ),
-                        ],
+   Widget _buildList(BuildContext context, List<Log> logsList) {
+    // Group logs by eatTime
+    Map<String, List<Log>> groupedLogs = {
+      'Breakfast': [],
+      'Lunch': [],
+      'Dinner': [],
+      'Snacks': [],
+    };
+
+    for (var log in logsList) {
+      if (groupedLogs.containsKey(log.eatTime)) {
+        groupedLogs[log.eatTime]!.add(log);
+      }
+    }
+
+    // Build the list view
+    return ListView(
+      children: groupedLogs.entries.expand<Widget>((entry) {
+        // Create a section for each eatTime
+        String eatTime = entry.key;
+        List<Log> logs = entry.value;
+
+        if (logs.isEmpty) return [];
+
+        return [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text(
+              eatTime,
+              style: const TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ...logs.map<Widget>((log) {
+            final bool isExpanded = expandedTile == logsList.indexOf(log);
+            return GestureDetector(
+              onTap: () => onTileTap(logsList.indexOf(log)),
+              child: AnimatedSize(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeOutQuint,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: isExpanded
+                            ? const Color.fromARGB(255, 255, 228, 141)
+                            : Colors.grey,
+                        width: 2.0,
                       ),
                     ),
                   ),
-                );
-              },
-            ),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        key: UniqueKey(),
+                        title: Text(
+                          log.name,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 255, 242, 199),
+                            shadows: <Shadow>[
+                              Shadow(
+                                blurRadius: 2,
+                                color: Color.fromARGB(255, 255, 228, 141),
+                              ),
+                            ],
+                          ),
+                        ),
+                        subtitle: Text(
+                          'Calories: ${log.calories.toString()}',
+                          maxLines: isExpanded ? null : 1,
+                          overflow: isExpanded
+                              ? TextOverflow.visible
+                              : TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 190, 190, 190),
+                          ),
+                        ),
+                        leading: isExpanded
+                          ? const Icon(
+                              Icons.keyboard_arrow_up_rounded,
+                              color: Color.fromARGB(255, 255, 196, 0),
+                              shadows: <Shadow>[
+                                Shadow(
+                                  blurRadius: 5,
+                                  color: Color.fromARGB(255, 255, 196, 0),
+                                ),
+                              ],
+                            )
+                          : const Icon(
+                              Icons.keyboard_arrow_down_rounded,
+                              color: Color.fromARGB(255, 255, 196, 0),
+                              shadows: <Shadow>[
+                                Shadow(
+                                  blurRadius: 5,
+                                  color: Color.fromARGB(255, 255, 196, 0),
+                                ),
+                              ],
+                            ),
+                        trailing: PopupMenuButton(
+                          iconColor: const Color.fromARGB(255, 255, 196, 0),
+                          onSelected: (value) {
+                            switch (value) {
+                              case 'open':
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => LogEntryView(log: log, appState: appState),
+                                  ),
+                                );
+                              break;
+                              case 'delete':
+                                appState.deleteLog(
+                                  log: log,
+                                  onSuccess: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(content: Text('Log was successfully deleted.')),
+                                    );
+                                  }
+                                );
+                              break;
+                            }
+                          },
+                          itemBuilder: (BuildContext bc) {
+                            return const [
+                              PopupMenuItem(
+                                value: 'open',
+                                child: Text("View Log"),
+                              ),
+                              PopupMenuItem(
+                                value: 'delete',
+                                child: Text("Delete"),
+                              ),
+                            ];
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+        ];
+      }).toList(),
     );
   }
+
 }
