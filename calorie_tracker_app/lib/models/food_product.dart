12b1e733bbdc2b3c9ae11d401b9a6048334b7322
IAmBamboo <ChimeiPho@gmail.com>
2024-08-11 04:26:48 -0600

    add imageurl

--- calorie_tracker_app/lib/models/food_product.dart
+++ calorie_tracker_app/lib/models/food_product.dart
@@ -4,6 +4,7 @@ class FoodProduct {
     required this.servingSize,
     required this.name,
     required this.quantity,
+    required this.imageUrl,
     required this.foodId,
     });
 
@@ -11,5 +12,6 @@ class FoodProduct {
   final int? calories;
   final String? servingSize;
   final String? quantity;
+  final String? imageUrl;
   final String? foodId; // Barcode ID for the food item used to find it in OpenFoodFact's database
 }
